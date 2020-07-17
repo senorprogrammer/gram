b4bd0d0059aeaa0d5db57f5340b2ff8a183fc1c2
Chris Cummer <ccummer@digitalocean.com>
2020-07-17 06:15:54 -0700

    Clean up the anagrammer for simplicity
    Signed-off-by: Chris Cummer <ccummer@digitalocean.com>

--- anagrammer/find.go
+++ anagrammer/find.go
@@ -14,7 +14,7 @@ const (
 
 // Find takes an input word and returns a set of all the anagrams for that word
 func Find(inputWord string) []string {
-	anagrams, err := findAnagrams(dictionaryPath, inputWord)
+	anagrams, err := scanForAnagrams(dictionaryPath, inputWord)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,50 +24,45 @@ func Find(inputWord string) []string {
 
 /* -------------------- Unexported Functions -------------------- */
 
-// findAnagrams uses the local dictionary to create an enumerable set of all known
+// scanForAnagrams uses the local dictionary to create an enumerable set of all known
 // words. This is used to check for anagrams
-func findAnagrams(dictPath string, inputWord string) ([]string, error) {
-	words := []string{}
+func scanForAnagrams(dictPath string, inputWord string) ([]string, error) {
+	anagrams := []string{}
 
 	dict, err := os.Open(dictPath)
 	if err != nil {
-		return words, err
+		return anagrams, err
 	}
 
 	scanner := bufio.NewScanner(dict)
 	for scanner.Scan() {
 		word := scanner.Text()
 
-		if len(word) != len(inputWord) {
-			// If the word we're scanning is a different length than the input word,
-			// this word cannot be an anagram of the input word, so don't include it in the dict
-			continue
-		}
-
-		if word == inputWord {
-			// If the word is the same word as the input word, it is not an anagram
-			continue
-		}
-
-		isAnagram := compareForEquality(inputWord, word)
-		if isAnagram {
-			words = append(words, word)
+		if wordsAreAnagrams(inputWord, word) {
+			anagrams = append(anagrams, word)
 		}
 	}
 
-	return words, nil
+	return anagrams, nil
 }
 
-// compareForEquality checks whether or not two strings, when sorted, are exactly the same
-func compareForEquality(a, b string) bool {
-	if len(a) != len(b) {
-		// If they don't have the same length, they cannot be the same
+// wordsAreAnagrams checks whether or not two strings, when sorted, are exactly the same
+func wordsAreAnagrams(wordA, wordB string) bool {
+	if len(wordA) != len(wordB) {
+		// If the word we're scanning is a different length than the input word,
+		// this word cannot be an anagram of the input word, so don't include it in the dict
 		return false
 	}
 
-	aSliced := strings.Split(a, "")
-	bSliced := strings.Split(b, "")
+	if wordA == wordB {
+		// If the word is the same word as the input word, it is not an anagram
+		return false
+	}
 
+	aSliced := strings.Split(wordA, "")
+	bSliced := strings.Split(wordB, "")
+
+	// Convert the slices to StringSlice so they can be sorted
 	sort.Sort(StringSlice(aSliced))
 	sort.Sort(StringSlice(bSliced))
 
@@ -75,7 +70,7 @@ func compareForEquality(a, b string) bool {
 		bLetter := bSliced[idx]
 
 		if aLetter != bLetter {
-			// If any letter differs positionally, they cannot be the same
+			// If any letter differs positionally, they cannot be anagrams
 			return false
 		}
 	}
